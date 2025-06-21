1656f263eefc79347679cf02aa1d2db5e00bc1b7
HeYu <code@qheyu.com>
2025-06-21 21:42:20 +0800

    迁移一些在用的 slicex 包方法

--- slicex/slicex.go
+++ slicex/slicex.go
@@ -1,11 +1,13 @@
 package slicex
 
-func Filter[T any, Slice ~[]T](s Slice, predicate func(T) bool) Slice {
+// Filter 过滤符合条件的元素生成新的slice
+// 与 slices.DeleteFunc() 不用，slicex.Filter 会产生新的slices而不是原地修改
+func Filter[S ~[]E, E any](s S, predicate func(E) bool) S {
 	if len(s) == 0 {
 		return nil
 	}
 
-	result := make(Slice, 0, len(s))
+	result := make(S, 0, len(s))
 	for _, item := range s {
 		if predicate(item) {
 			result = append(result, item)
@@ -14,14 +16,34 @@ func Filter[T any, Slice ~[]T](s Slice, predicate func(T) bool) Slice {
 	return result
 }
 
-func Map[T any, Slice ~[]T, R any](s Slice, transform func(T) R) []R {
+func Map[S ~[]E1, E1 any, E2 any](s S, transform func(E1) E2) []E2 {
 	if len(s) == 0 {
 		return nil
 	}
 
-	result := make([]R, len(s))
+	result := make([]E2, len(s))
 	for i, v := range s {
 		result[i] = transform(v)
 	}
 	return result
+}
+
+// 类似 slices.Index()，但是是反向开始查找的
+func LastIndex[S ~[]E, E comparable](s S, v E) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		if v == s[i] {
+			return i
+		}
+	}
+	return -1
+}
+
+// 类似 slices.IndexFunc()，但是是反向开始查找的
+func LastIndexFunc[S ~[]E, E any](s S, f func(E) bool) int {
+	for i := len(s) - 1; i >= 0; i-- {
+		if f(s[i]) {
+			return i
+		}
+	}
+	return -1
 }
