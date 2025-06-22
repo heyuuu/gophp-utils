6a0f4a2e2ac298bc1ae0e65f593c0a03fc1606e7
HeYu <code@qheyu.com>
2025-06-22 17:29:09 +0800

    迁移一些在用的 xstrings 包方法

--- xstrings/xstrings.go
+++ xstrings/xstrings.go
@@ -1,12 +1,51 @@
 package xstrings
 
-import "unicode"
+import (
+	"github.com/heyuuu/gophp-kits/ascii"
+	"strings"
+)
 
 func IsBlank(s string) bool {
 	for _, r := range s {
-		if !unicode.IsSpace(r) {
+		if !ascii.IsSpace(r) {
 			return false
 		}
 	}
 	return true
+}
+
+func PadLeft(s string, size int, pad byte) string {
+	if len(s) >= size {
+		return s
+	}
+
+	buf := make([]byte, size)
+	padSize := size - len(s)
+	copy(buf[:padSize], s)
+	for i := 0; i < padSize; i++ {
+		buf[i] = pad
+	}
+	return string(buf)
+}
+
+func PadRight(s string, size int, pad byte) string {
+	if len(s) >= size {
+		return s
+	}
+
+	buf := make([]byte, size)
+	copy(buf, s)
+	for i := len(s); i < size; i++ {
+		buf[i] = pad
+	}
+	return string(buf)
+}
+
+// LastCut
+// 类似 strings.Cut()，但是是从字符串尾部反向开始查找的
+func LastCut(s string, sep string) (before, after string, found bool) {
+	if i := strings.LastIndex(s, sep); i >= 0 {
+		return s[:i], s[i+len(sep):], true
+	}
+	return s, "", false
 }
